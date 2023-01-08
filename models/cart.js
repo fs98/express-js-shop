7ed7ccdecef6264278e90682cc9f4197e7c9e7fd
Fata Sefer <sefer.fata@gmail.com>
2023-01-08 20:00:44 +0100

    feat: add fetchAll method in Cart class

--- models/cart.js
+++ models/cart.js
@@ -7,7 +7,21 @@ const p = path.join(
   "cart.json"
 );
 
+const getCartProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Cart {
+  static fetchAll(cb) {
+    getCartProductsFromFile(cb);
+  }
+
   static addProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
@@ -36,6 +50,31 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(cart), (err) => {
         console.log(err);
       });
+    });
+  }
+
+  static deleteCartProduct(productId, price) {
+    getCartProductsFromFile((cart) => {
+      const product = cart.products.find((product) => product.id === productId);
+
+      if (product) {
+        const updatedCartProducts = cart.products.filter(
+          (product) => product.id !== productId
+        );
+
+        const updatedCartTotalPrice = cart.totalPrice - product.qty * price;
+
+        const updatedCart = {
+          products: updatedCartProducts,
+          totalPrice: updatedCartTotalPrice,
+        };
+
+        fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+          console.log(err);
+        });
+      } else {
+        console.log("Not in the list.");
+      }
     });
   }
 };
